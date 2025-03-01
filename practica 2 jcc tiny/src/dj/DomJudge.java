35c6ea0ebdfae433afdbef8c2283798702895c91
Allera01 <allera01@ucm.es>
2025-03-01 17:29:20 +0100

    cambio menor

--- practica 2 jcc tiny/src/dj/DomJudge.java
+++ practica 2 jcc tiny/src/dj/DomJudge.java
@@ -4,15 +4,13 @@ import asint.AnalizadorSintacticoTinyDJ;
 import asint.ParseException;
 import asint.TokenMgrError;
 
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.InputStreamReader;
-import java.io.Reader;
 public class DomJudge{
    public static void main(String[] args) throws Exception {
      try{  
       AnalizadorSintacticoTiny asint = new AnalizadorSintacticoTinyDJ(new InputStreamReader(System.in));
-      //Reader input = new InputStreamReader(new FileInputStream("08.in"));
+      //Reader input = new InputStreamReader(new FileInputStream("sample3.in"));
 	  //AnalizadorSintacticoTiny asint = new AnalizadorSintacticoTinyDJ(input);
       asint.programa();
      }
